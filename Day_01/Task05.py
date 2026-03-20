b0fdb3fc403bcb5916a27c7caed55b199f8984b5
vivs-ty <vivesht07@gmail.com>
2026-03-20 14:14:37 +0530

    day 1 task 05

---
+++ Day_01/Task05.py
@@ -0,0 +1,7 @@
+# Task 5: Convert a temperature from Celsius to Fahrenheit and display it with two decimal places.
+
+celsius = float(input("Enter temperature in Celsius: ").strip())
+fahrenheit = (celsius * 9/5) + 32
+print(f" \n The temperature in Fahrenheit is : {fahrenheit:.2f} \n")
+print(f" \n Python 30 days Series - Day 1 Task 5 \n")
+print(f" \n Have a good one! ")
