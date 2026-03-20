0a0fd9e16087c2aa71f1dcb8a4891af516ce7e3b
vivs-ty <vivesht07@gmail.com>
2026-03-20 14:46:46 +0530

    day 1 task 6

---
+++ Day_01/Task06.py
@@ -0,0 +1,40 @@
+# Task 6: Build a simple command-line calculator that supports +, -, *, and /.
+
+def calculator():
+    print("Welcome to the Simple Command-Line Calculator!")
+    print("You can perform operations: +, -, *, /, %,")
+    
+    num1 = float(input("Enter the first number: ").strip())
+    operator = input("Enter the operator (+, -, *, /): ").strip()
+    num2 = float(input("Enter the second number: ").strip())
+    
+    if operator == '+':
+        result = num1 + num2
+        print(f"The result of {num1} + {num2} is: {result}")
+    elif operator == '-':
+        result = num1 - num2
+        print(f"The result of {num1} - {num2} is: {result}")
+    elif operator == '*':
+        result = num1 * num2
+        print(f"The result of {num1} * {num2} is: {result}")
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+            print(f"The result of {num1} / {num2} is: {result}")
+        else:
+            print("Error: Division by zero is undefined.")
+    elif operator == '%':
+        if num2 != 0:
+            result = (num1 * 100) / num2
+            print(f"The result of {num1} % {num2} is: {result}")
+        else:
+            print("Error: Division by zero is undefined.")
+    else:
+        print("Invalid operator. Please use one of +, -, *, /, %.")
+    
+    print(f" \n Python 30 days Series - Day 1 Task 6 \n")
+    print(f" \n Have a good one! ")
+
+
+if __name__ == "__main__":
+        calculator()
