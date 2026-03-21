ca9a3487b960f66bc0475b7d60268f41eae6bf1d
vivs-ty <vivesht07@gmail.com>
2026-03-21 22:35:30 +0530

    Day 2 task 11

---
+++ Day_02/Task11.py
@@ -0,0 +1,10 @@
+# Task 11: Check whether a year is a leap year.
+
+year = int(input("Enter a Year: ").strip())
+if ( year % 4 == 0 and year % 100 != 0 ) or ( year % 400 == 0 ):
+    print(f"Entered year {year} is a Leap Year")
+else:
+    print(f"Entered year {year} is not a Leap Year")
+
+print(f" \n Python 30 days Series - Day 2 Task 11 \n")
+print(f" \n Have a good one! ")
