3b5248ed90305c6cc6b8417b62e4ce9a0cd36c03
vivs-ty <vivesht07@gmail.com>
2026-03-21 22:14:34 +0530

    Day 2 task 7

---
+++ Day_02/Task07.py
@@ -0,0 +1,10 @@
+# Task 7: Check whether a number is even or odd.
+
+number = int(input("Enter a Number: ").strip())
+if number % 2 ==0 :
+    print(f"Entered number {number} is Even")
+else:
+    print(f"Entered number {number} is Odd")
+
+print(f" \n Python 30 days Series - Day 2 Task 7 \n")
+print(f" \n Have a good one! ")
