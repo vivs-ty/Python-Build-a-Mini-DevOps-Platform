0f5b53db6aece3c42ae27a1a917c6928c11e2439
vivs-ty <vivesht07@gmail.com>
2026-03-21 22:20:03 +0530

    day 2 task 8

---
+++ Day_02/Task08.py
@@ -0,0 +1,12 @@
+# Task 8: Check whether a number is positive, negative, or zero.
+
+number = float(input("Enter a Number: ").strip())
+if number > 0:
+    print(f"Entered number {number} is Positive")
+elif number < 0:
+    print(f"Entered number {number} is Negative")
+else:
+    print(f"Entered number {number} is Zero")
+
+print(f" \n Python 30 days Series - Day 2 Task 8 \n")
+print(f" \n Have a good one! ")
