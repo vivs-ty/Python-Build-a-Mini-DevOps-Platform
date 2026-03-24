20a0ff3af95b9b8b307fc5f12326dd435102148c
vivs-ty <vivesht07@gmail.com>
2026-03-24 21:49:04 +0530

    Day 3 Task 15

---
+++ Day_03/Task15.py
@@ -0,0 +1,14 @@
+# Task 15: Calculate the factorial of a number using a loop.
+
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result  
+print(f" \n Python 30 days Series - Day 3 Task 15 \n")
+print(f" \n Have a good one! \n")
