11137b6a3d43c7413c81555e629ec882299722ee
vivs-ty <vivesht07@gmail.com>
2026-03-24 21:47:13 +0530

    Day 3 Task14

---
+++ Day_03/Task14.py
@@ -0,0 +1,8 @@
+#  Task 14: Generate a multiplication table for a user-provided number.
+
+number = int(input("Enter a number to generate it's multiplication table: ").strip())
+print(f"\n You have entered {number} \n Multiplication Table for {number}:\n")
+for i in range(1, 21):
+    print(f"{number} x {i} = {number * i}")
+print(f" \n Python 30 days Series - Day 3 Task 14 \n")
+print(f" \n Have a good one! \n")
