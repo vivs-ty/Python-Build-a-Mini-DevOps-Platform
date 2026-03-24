d5298a096e863942d1cdab481ada788c80585457
vivs-ty <vivesht07@gmail.com>
2026-03-24 21:51:36 +0530

    Day 3 Task 17

---
+++ Day_03/Task17.py
@@ -0,0 +1,8 @@
+#  Task 17: Count the number of digits in an integer.
+
+def count_digits(n):
+    if n < 0:
+        n = -n  # Make it positive if it's negative
+    return len(str(n))
+print(f" \n Python 30 days Series - Day 3 Task 17 \n")
+print(f" \n Have a good one! \n")
