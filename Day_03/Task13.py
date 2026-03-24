cd172bc9b3eade5fb6a09fe7b48250f95b829dba
vivs-ty <vivesht07@gmail.com>
2026-03-24 21:39:46 +0530

    Day 3 Task 13

---
+++ Day_03/Task13.py
@@ -0,0 +1,9 @@
+# Task 13: Print numbers from 1 to 100 and skip values divisible by 3.
+
+for i in range(1, 100):
+    if i % 3 == 0:
+        continue
+    print(i)
+print(f" \n Python 30 days Series - Day 3 Task 13 \n")
+print(f" \n Have a good one! \n")
+
