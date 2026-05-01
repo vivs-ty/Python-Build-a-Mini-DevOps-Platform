471bdc65c1539bbee49300f40ca01cd99cdcc638
vivs-ty <vivesht07@gmail.com>
2026-05-01 21:15:08 +0530

    Task 19

---
+++ Day_04/Task19.py
@@ -0,0 +1,11 @@
+# Task 19: Reverse a string without using built-in reverse helpers.
+
+input_string = input("Enter a string to reverse: ").strip()
+reversed_string = ""
+for char in input_string:
+    reversed_string = char + reversed_string
+print(f"Reversed string: {reversed_string}")
+
+print(f" \n Python 30 days Series - Day 4 Task 19 \n") 
+print(f" \n Day 4: Strings \n")
+print(f" \n Have a good one! \n")
