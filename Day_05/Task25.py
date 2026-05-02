bd6ff8bd991a0d3249ea168a8b4120add11f6073
vivs-ty <vivesht07@gmail.com>
2026-05-02 21:36:18 +0530

    Task 25

---
+++ Day_05/Task25.py
@@ -0,0 +1,8 @@
+# Task 25: Remove duplicate elements from a list.
+
+input_list = input("Enter a list of elements separated by spaces: ").split()
+result = list(dict.fromkeys(input_list))
+print(f"List after removing duplicates: {result}")
+print(f" \n Python 30 days Series - Day 5 Task 25 \n")
+print(f" \n Day 5: Lists, Sets, and Dictionaries \n")
+print(f" \n Have a good one! \n")
