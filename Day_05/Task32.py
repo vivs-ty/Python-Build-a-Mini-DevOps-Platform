103d1dba922395b90172cf736e2de77cda318f75
vivs-ty <vivesht07@gmail.com>
2026-05-02 21:45:34 +0530

    Task 32

---
+++ Day_05/Task32.py
@@ -0,0 +1,11 @@
+# Task 32: Find the union and intersection of two sets.
+
+set1 = set(input("Enter the first set of elements separated by spaces: ").split())
+set2 = set(input("Enter the second set of elements separated by spaces: ").split())
+union = set1.union(set2)
+intersection = set1.intersection(set2)
+print(f"Union: {union}")
+print(f"Intersection: {intersection}")
+print(f" \n Python 30 days Series - Day 5 Task 32 \n")
+print(f" \n Day 5: Lists, Sets, and Dictionaries \n")
+print(f" \n Have a good one! \n")
