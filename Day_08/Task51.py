9cef3a8344c7610acaa819ab1b2bb8298e1ac4e3
vivs-ty <vivesht07@gmail.com>
2026-05-05 21:39:16 +0530

    Task 51

---
+++ Day_08/Task51.py
@@ -0,0 +1,25 @@
+# Task 51: Create LinuxServer and WindowsServer classes that inherit from Server.
+
+class Server:
+    def __init__(self, hostname: str, ip_address: str):
+        self.hostname = hostname
+        self.ip_address = ip_address
+
+# LinuxServer inherits from Server
+class LinuxServer(Server):
+    def __init__(self, hostname: str, ip_address: str, distro: str):
+        super().__init__(hostname, ip_address) # Call parent constructor
+        self.distro = distro
+
+# WindowsServer inherits from Server
+class WindowsServer(Server):
+    def __init__(self, hostname: str, ip_address: str, license_key: str):
+        super().__init__(hostname, ip_address)
+        self.license_key = license_key
+
+# Demonstration
+ubuntu_node = LinuxServer("worker-1", "192.168.1.50", "Ubuntu 22.04")
+win_node = WindowsServer("ad-primary", "192.168.1.100", "WIN-XYZ-123")
+
+print(f"Linux Node: {ubuntu_node.hostname} running {ubuntu_node.distro}")
+print(f"Windows Node: {win_node.hostname} with IP {win_node.ip_address}")
