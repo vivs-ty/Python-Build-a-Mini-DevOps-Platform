42bab689192d3843015d1b0c46236e7c390db935
vivs-ty <vivesht07@gmail.com>
2026-05-07 22:00:48 +0530

    Day 3

--- Day_03/Task14.py
+++ Day_03/Task14.py
@@ -6,4 +6,4 @@ for i in range(1, 21):
     print(f"{number} x {i} = {number * i}")
 print(f" \n Python 30 days Series - Day 3 Task 14 \n")
 print(f" \n Day 3: Loops \n")
-print(f" \n Have a good one! \n")
+print(f" \n Have a good one! \n " + "-"*40)
