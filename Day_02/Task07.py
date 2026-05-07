2cda77fb6125b8235af07cae3fa663405a393d97
vivs-ty <vivesht07@gmail.com>
2026-05-07 22:02:36 +0530

    Day 2

--- Day_02/Task07.py
+++ Day_02/Task07.py
@@ -8,4 +8,4 @@ else:
 
 print(f" \n Python 30 days Series - Day 2 Task 7 \n")
 print(f" \n Day 2: Conditional Logic \n")
-print(f" \n Have a good one! ")
+print(f" \n Have a good one! \n  " + "-"*40)
