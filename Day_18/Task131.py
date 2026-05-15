428dbf523270138d990e35a505ab3d68537723d9
vivs-ty <vivesht07@gmail.com>
2026-05-15 22:00:49 +0530

    Task 131

---
+++ Day_18/Task131.py
@@ -0,0 +1,18 @@
+# Task 131: Build a file organizer that moves files by file type.
+
+import os
+import shutil
+
+def organize_files(directory):
+    for file in os.listdir(directory):
+        filepath = os.path.join(directory, file)
+        
+        if os.path.isfile(filepath):
+            extension = file.split('.')[-1]
+            target_folder = os.path.join(directory, extension)
+            
+            if not os.path.exists(target_folder):
+                os.mkdir(target_folder)
+                
+            shutil.move(filepath, os.path.join(target_folder, file))
+            print(f"Moved {file} to {extension} folder")
