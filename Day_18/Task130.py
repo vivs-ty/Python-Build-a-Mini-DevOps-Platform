8d6750a7591cc6d1354ad36e260052eacc747494
vivs-ty <vivesht07@gmail.com>
2026-05-15 22:00:37 +0530

    Task 130

---
+++ Day_18/Task130.py
@@ -0,0 +1,23 @@
+# Task 130: Monitor a directory and log create, delete, and modify events.
+
+import os
+import time
+
+def monitor_directory(directory):
+    print(f"Monitoring {directory}...")
+    seen_files = set(os.listdir(directory))
+    
+    while True:
+        time.sleep(2)
+        current_files = set(os.listdir(directory))
+        
+        added = current_files - seen_files
+        removed = seen_files - current_files
+        
+        for file in added:
+            print(f"Created: {file}")
+        for file in removed:
+            print(f"Deleted: {file}")
+            
+        seen_files = current_files
+        
