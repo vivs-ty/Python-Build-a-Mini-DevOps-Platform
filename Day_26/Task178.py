2580b3c7d2782fd46c6d51cf67aa9e9afb194664
vivs-ty <vivesht07@gmail.com>
2026-05-24 22:15:32 +0530

    Task 178

--- Day_26/Task178.py
+++ Day_26/Task178.py
@@ -1,12 +1,27 @@
 # Task 178: List all instances or resources in a cloud environment.
 
 import boto3
+from botocore.exceptions import ClientError
 
-ec2 = boto3.client('ec2')
+def list_ec2_instances():
+    try:
+        ec2 = boto3.client('ec2')
+        response = ec2.describe_instances()
+        
+        count = 0
+        for reservation in response.get('Reservations', []):
+            for instance in reservation.get('Instances', []):
+                instance_id = instance['InstanceId']
+                state = instance['State']['Name']
+                print(f"Instance ID: {instance_id}, State: {state}")
+                count += 1
+                
+        print(f"Total instances found: {count}")
+        
+    except ClientError as e:
+        print(f"AWS Error: {e}")
 
-response = ec2.describe_instances()
-for reservation in response['Reservations']:
-    for instance in reservation['Instances']:
-        print(f"Instance ID: {instance['InstanceId']}, State: {instance['State']['Name']}")
-
-print(f"Total instances found: {len([instance for reservation in response['Reservations'] for instance in reservation['Instances']])}")
+if __name__ == "__main__":
+    list_ec2_instances()
+    
+   
