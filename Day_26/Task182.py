e3976c2844717fca8ad08aa825dc602fe8d5d306
vivs-ty <vivesht07@gmail.com>
2026-05-24 22:20:49 +0530

    Task182

--- Day_26/Task182.py
+++ Day_26/Task182.py
@@ -1,17 +1,31 @@
 # Task 182: Delete unused cloud resources to optimize cost.
 
 import boto3
+from botocore.exceptions import ClientError
 
-ec2 = boto3.client('ec2')
-response = ec2.describe_instances()
-for reservation in response['Reservations']:
-    for instance in reservation['Instances']:
-        instance_id = instance['InstanceId']
-        state = instance['State']['Name']
-        print(f"Instance ID: {instance_id}, State: {state}")
+def terminate_stopped_instances():
+    try:
+        ec2 = boto3.client('ec2')
+        response = ec2.describe_instances()
         
-        # Example: Terminate the instance if it's stopped
-        if state == 'stopped':
-            print(f"Terminating instance {instance_id}...")
-            ec2.terminate_instances(InstanceIds=[instance_id])
-print(f"Total instances processed for deletion: {len([instance for reservation in response['Reservations'] for instance in reservation['Instances'] if instance['State']['Name'] == 'stopped'])}")  
+        terminated_count = 0
+        for reservation in response.get('Reservations', []):
+            for instance in reservation.get('Instances', []):
+                instance_id = instance['InstanceId']
+                state = instance['State']['Name']
+                
+                # Terminate the instance if it is currently stopped
+                if state == 'stopped':
+                    print(f"Terminating stopped instance {instance_id}...")
+                    ec2.terminate_instances(InstanceIds=[instance_id])
+                    terminated_count += 1
+                    
+        print(f"Total stopped instances terminated: {terminated_count}")
+        
+    except ClientError as e:
+        print(f"AWS Error: {e}")
+
+if __name__ == "__main__":
+    terminate_stopped_instances()
+    
+   
