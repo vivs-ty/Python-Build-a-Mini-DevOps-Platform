b268acd3defb67387cde30fb9a66e50c73152376
vivs-ty <vivesht07@gmail.com>
2026-05-24 22:17:45 +0530

    Task180

--- Day_26/Task180.py
+++ Day_26/Task180.py
@@ -2,14 +2,35 @@
 
 import boto3
 import logging
+from botocore.exceptions import ClientError
 
-# Configure logging
-logging.basicConfig(filename='cloud_resource_usage.log', level=logging.INFO, format='%(asctime)s - %(message)s')
-ec2 = boto3.client('ec2')
-response = ec2.describe_instances()
-for reservation in response['Reservations']:
-    for instance in reservation['Instances']:
-        instance_id = instance['InstanceId']
-        state = instance['State']['Name']
-        logging.info(f"Instance ID: {instance_id}, State: {state}")
-print(f"Total instances monitored: {len([instance for reservation in response['Reservations'] for instance in reservation['Instances']])}")
+# Configure logging to write directly to a file
+logging.basicConfig(
+    filename='cloud_resource_usage.log', 
+    level=logging.INFO, 
+    format='%(asctime)s - %(message)s'
+)
+
+def log_instance_states():
+    try:
+        ec2 = boto3.client('ec2')
+        response = ec2.describe_instances()
+        
+        count = 0
+        for reservation in response.get('Reservations', []):
+            for instance in reservation.get('Instances', []):
+                instance_id = instance['InstanceId']
+                state = instance['State']['Name']
+                
+                logging.info(f"Instance ID: {instance_id}, State: {state}")
+                count += 1
+                
+        print(f"Logged states for {count} instances to 'cloud_resource_usage.log'")
+        
+    except ClientError as e:
+        print(f"AWS Error: {e}")
+
+if __name__ == "__main__":
+    log_instance_states()
+    
+    
